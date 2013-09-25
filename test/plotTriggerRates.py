9f22099756715ee94d9ebf7bcaddb73b9fda302c
Mark Grimes <grimes@cern.ch>
2013-09-25 18:53:57 +0100

    Tidied up the plotting classes

--- test/plotTriggerRates.py
+++ test/plotTriggerRates.py
@@ -7,197 +7,234 @@ import math
 
 #gROOT.Reset()
 
-class TriggerRateComparison(object):
+class TriggerRate(object):
 	"""
 	Class that will plot one or more trigger rate histograms into the TPad provided.
 	
 	Author Mark Grimes (mark.grimes@bristol.ac.uk)
 	Date 02/Sep/2013
 	"""
-	def __init__( self, pad, histograms ) :
-		# allow simple passing of a single histogram by converting to a list if it's not already
-		try: len(histograms)
-		except: histograms=[histograms]
-		
-		# make a copy of all of the histograms, rather than modifying the originals
-		self.histograms=[]
-		for histogram in histograms :
-			self.histograms.append( histogram.Clone() )
-			self.histograms[-1].SetDirectory( 0 ) # Make sure the copy isn't saved to disk
-		
-		self.pad=pad
-		self.pad.SetGrid()
-		self.pad.cd()
-		self.pad.SetLogy()
-		
-	def draw( self ) :
-		self.pad.cd()
+	instantiationCount = 0 # Need this to get around root's stupid requirement for globally unique names
+	
+	def __init__( self, pad ) :
+		self.instantiationCount=TriggerRate.instantiationCount
+		TriggerRate.instantiationCount+=1
 
-class TriggerRatePlot:
-	"""Class that takes two trigger rate histograms, plots them and plots the ratio of the two underneath"""
-	instantiationCount = 0
-	canvas = None
-	ratioPad = None
-	ratePad = None
-	ratio = None
-	numerator = None
-	denominator = None
-	legend = None
-	
-	def __init__( self, titleAndFile1, titleAndFile2, triggerName ) :
-		file1 = titleAndFile1[1]
-		file2 = titleAndFile2[1]
-		title1 = titleAndFile1[0]
-		title2 = titleAndFile2[0]
-		self.instantiationCount=TriggerRatePlot.instantiationCount
-		TriggerRatePlot.instantiationCount+=1
-		
-		self.numerator=self.loadHistogram( file1, triggerName ).Clone()
-		self.numerator.SetName( self.numerator.GetName()+"_numerator"+str(self.instantiationCount) )
-		self.numerator.SetDirectory( 0 )
-		if file2!=None :
-			self.denominator=self.loadHistogram( file2, triggerName ).Clone()
-			self.denominator.SetName( self.denominator.GetName()+"_denominator"+str(self.instantiationCount) )
-			self.denominator.SetDirectory( 0 )
-		
-		
-		splitPoint=0.33
-		padRatio=(1-splitPoint)/splitPoint
-		
-		self.canvas=TCanvas()
-		if self.denominator!=None : 
-			self.ratePad=TPad( "ratePad"+str(self.instantiationCount), "L1 rate", 0, splitPoint, 1, 1 )
-			self.ratioPad=TPad( "ratioPad"+str(self.instantiationCount), "L1 ratio", 0, 0, 1, splitPoint )
-			self.ratioPad.SetGrid();
-		else :
-			self.ratePad=TPad( "ratePad"+str(self.instantiationCount), "L1 rate", 0, 1, 1, 1 )
-			
-		#self.numerator.GetXaxis().SetNdivisions(520);
-		#self.numerator.GetYaxis().SetNdivisions(520);
-		self.ratePad.SetGrid();
-		
-		if self.ratioPad!=None:
-			self.ratioPad.SetTopMargin(0)
-			self.ratioPad.SetBottomMargin( self.ratePad.GetBottomMargin()*padRatio*1.1 ) # A little 10% extra because some is cut off
-		self.ratePad.SetBottomMargin(0.1) # was zero before I wanted to see the axis
-		self.ratePad.Draw()
-		if self.ratioPad!=None: self.ratioPad.Draw()
-		self.ratePad.cd()
-		
-		self.ratePad.SetLogy()
-#		histogramTitle=self.numerator.GetTitle()
-#		try:
-#			titleSplit=histogramTitle.split("_")
-#			title=titleSplit[1];
-#			for section in titleSplit[2:-1]: title=title+"_"+section
-#			self.numerator.SetTitle( title )
-#		except:
-#			pass # leave the title as it is
-		self.numerator.GetYaxis().SetTitle( "Rate/kHz" )
-		self.numerator.SetStats(0)
-		self.numerator.SetLineColor(2)
-		self.numerator.SetMarkerColor(2)
-		#self.numerator.GetYaxis().SetTitleSize( self.numerator.GetYaxis().GetTitleSize()*1.5 )
-		#self.numerator.GetXaxis().SetTitleSize( self.numerator.GetXaxis().GetTitleSize()*1.5 )
-		self.numerator.GetYaxis().SetTitleSize( 0.053 );
-		self.numerator.GetXaxis().SetTitleSize( 0.053 );
-		delayFirstDraw=False
-		if self.denominator!=None :
-			self.denominator.SetTitle( self.numerator.GetTitle() )
-			self.denominator.GetYaxis().SetTitle( self.numerator.GetYaxis().GetTitle() )
-			self.denominator.GetYaxis().SetTitleSize( self.numerator.GetYaxis().GetTitleSize() );
-			self.denominator.GetXaxis().SetTitleSize( self.numerator.GetXaxis().GetTitleSize() );
-			if self.denominator.GetBinContent(1) > self.numerator.GetBinContent(1) : delayFirstDraw=True
-		if not delayFirstDraw : self.numerator.Draw()
-		
-		if self.denominator!=None :
-			self.denominator.SetStats(0)
-			self.denominator.SetLineColor(4)
-			self.denominator.SetMarkerColor(4)
-			if delayFirstDraw :
-				self.denominator.Draw()
-				self.numerator.Draw("same")
-			else :
-				self.denominator.Draw("same")
-	
-		
-			self.ratio = TH1D( "ratio"+str(self.instantiationCount), "", self.numerator.GetXaxis().GetNbins(), self.numerator.GetXaxis().GetXmin(), self.numerator.GetXaxis().GetXmax()  )
-			self.ratio.SetDirectory( 0 )
-			for binNumber in range( 1, self.numerator.GetXaxis().GetNbins()+1 ):
-				#print self.numerator.GetBinContent(binNumber),"/",self.denominator.GetBinContent(binNumber),"=",self.numerator.GetBinContent(binNumber)/self.denominator.GetBinContent(binNumber)
-				try : 
-					rateRatio=self.numerator.GetBinContent(binNumber)/self.denominator.GetBinContent(binNumber)
-					error=rateRatio*math.sqrt( math.pow(self.numerator.GetBinError(binNumber)/self.numerator.GetBinContent(binNumber),2) + math.pow(self.denominator.GetBinError(binNumber)/self.denominator.GetBinContent(binNumber),2) )
-				except ZeroDivisionError:
-					rateRatio = 0
-					error = 0
-				self.ratio.SetBinContent( binNumber, rateRatio )
-				self.ratio.SetBinError( binNumber, error )
-		
-			self.ratioPad.cd()
-			self.ratio.SetStats(0)
-			self.ratio.GetXaxis().SetLabelSize( self.numerator.GetXaxis().GetLabelSize()*padRatio )
-			self.ratio.GetYaxis().SetLabelSize( self.numerator.GetYaxis().GetLabelSize()*padRatio )
-			self.ratio.GetXaxis().SetTitle( "Threshold" )
-			self.ratio.GetYaxis().SetTitle( "Ratio " )
-			self.ratio.GetXaxis().SetTitleSize( self.numerator.GetXaxis().GetTitleSize()*padRatio )
-			self.ratio.GetYaxis().SetTitleSize( self.numerator.GetYaxis().GetTitleSize()*padRatio )
-			self.ratio.GetYaxis().SetTitleOffset( self.numerator.GetYaxis().GetTitleOffset()/padRatio )
-			self.ratio.Draw()
-		
-		self.ratePad.cd()
+		self.histograms=[]
+		# Create a legend. This might not actually be plotted if drawLegend is changed to False
 		self.legend=TLegend( 0.59, 0.72*0.9, 0.99, 0.95*0.9 )
 		self.legend.SetTextSize( 0.04 )
-		self.legend.AddEntry( self.numerator, title1 )
-		if self.denominator!=None : self.legend.AddEntry( self.denominator, title2 )
-		self.legend.Draw()
-	
-	def loadHistogram( self, file, triggerName ) :
-		histogram=file.Get( "h_"+triggerName+"_byThreshold" )
-		if histogram != None : return histogram
-		
-		histogram=file.Get( "L1_"+triggerName+"_v_allThresholdsScaled" )
-		if histogram != None : return histogram
-		
-		histogram=file.Get( "L1_"+triggerName+"_v_threshold1" )
-		if histogram != None : return histogram
-		
-		print "Couldn't load file for trigger "+triggerName
-		histogram=file.Get( "L1_SingleEG_v_threshold1" )
-		if histogram != None : return histogram
+		self.drawLegend=True
+		self.pad=pad
+		#self.pad.cd()
+		self.pad.SetGrid()
+		self.pad.SetLogy()
+		self.maxiumumBinHeight=0
+		self.axisLabelSize=None
+		self.axisTitleSize=None
+		self.axisTitleOffset=None
 
-		file.ls()
-		raise Exception( "Coudln't load the histogram for trigger "+triggerName )
-	
+	def add( self, histogram, legendTitle=None ):
+		availableColours=[2,4,5,6,7,8,9,10]
+		if legendTitle==None: legendTitle=histogram.GetTitle()
+		self.histograms.append( histogram.Clone() )
+		# Make sure the copy has a globally unique name
+		self.histograms[-1].SetName( self.histograms[-1].GetName()+"_hist"+str(len(self.histograms))+"inst"+str(self.instantiationCount) )
+		self.histograms[-1].SetDirectory( 0 ) # Make sure the copy isn't saved to disk
+		self.histograms[-1].SetLineColor( availableColours[(len(self.histograms)-1)%len(availableColours)] )
+		self.histograms[-1].SetMarkerColor( availableColours[(len(self.histograms)-1)%len(availableColours)] )
+		self.histograms[-1].SetStats(0)
+		if self.axisLabelSize==None:
+			self.axisLabelSize=self.histograms[-1].GetXaxis().GetLabelSize()
+		if self.axisTitleSize==None:
+			self.axisTitleSize=self.histograms[-1].GetXaxis().GetTitleSize()
+		if self.axisTitleOffset==None:
+			self.axisTitleOffset=self.histograms[-1].GetYaxis().GetTitleOffset()
+		self.histograms[-1].GetYaxis().SetTitle( "Rate/kHz" )
+		self.legend.AddEntry( self.histograms[-1], legendTitle )
+		# See if the histogram maximum is larger than the current maximum
+		bin=self.histograms[-1].GetMaximumBin()
+		try: max=self.histograms[-1].GetBinContent(bin)+self.histograms[-1].GetBinErrorUp(bin)
+		except: max=self.histograms[-1].GetBinContent(bin)+self.histograms[-1].GetBinError(bin)
+		if max>self.maxiumumBinHeight: self.maxiumumBinHeight=max
+		
 	def draw( self ) :
-		self.canvas.cd()
+		self.pad.cd()
+		for index in range(0,len(self.histograms)):
+			self.histograms[index].SetMaximum( self.maxiumumBinHeight*1.2 )
+			self.histograms[index].GetYaxis().SetTitleSize( self.axisTitleSize );
+			self.histograms[index].GetYaxis().SetTitleOffset( self.axisTitleOffset );
+			self.histograms[index].GetXaxis().SetTitleSize( self.axisTitleSize );
+			if index==0: self.histograms[index].Draw()
+			else: self.histograms[index].Draw("same")
+		if self.drawLegend: self.legend.Draw()
+		self.pad.Update()
+
+class TriggerRateRatio(object):
+	"""
+	Class that will plot a ratio between two histograms into the TPad provided.
+	Author Mark Grimes (mark.grimes@bristol.ac.uk)
+	Date 14/Sep/2013
+	"""
+	instantiationCount = 0 # Need this to get around root's stupid requirement for globally unique names
+	
+	def __init__( self, pad ):
+		self.instantiationCount=TriggerRateRatio.instantiationCount
+		TriggerRateRatio.instantiationCount+=1
+
+		self.ratioHistograms=[]
+		self.maxiumumBinHeight=0;
+		self.minimumBinHeight=999999999;
+
+		self.pad=pad
+		self.pad.SetGrid()
+		
+		self.axisLabelSize=None
+		self.axisTitleSize=None
+		self.axisTitleOffset=None
+	
+	def add( self, numeratorHistogram, denominatorHistogram, legendTitle=None ):
+		availableColours=[1,2,4,5,6,7,8,9,10]
+		
+		numberOfBins=numeratorHistogram.GetXaxis().GetNbins()
+		lowEdge=numeratorHistogram.GetXaxis().GetXmin()
+		highEdge=numeratorHistogram.GetXaxis().GetXmax()
+		self.ratioHistograms.append( TH1D( "TriggerRateRatio"+str(self.instantiationCount)+"_ratio", "", numberOfBins, lowEdge, highEdge ) )
+		self.ratioHistograms[-1].SetDirectory( 0 ) # Make sure the copy isn't saved to disk
+		self.ratioHistograms[-1].SetLineColor( availableColours[(len(self.ratioHistograms)-1)%len(availableColours)] )
+		self.ratioHistograms[-1].SetMarkerColor( availableColours[(len(self.ratioHistograms)-1)%len(availableColours)] )
+		self.ratioHistograms[-1].SetStats(0)
+		self.ratioHistograms[-1].GetXaxis().SetTitle( "Threshold" )
+		self.ratioHistograms[-1].GetYaxis().SetTitle( "Ratio " )
+		#self.ratioHistograms[-1].GetYaxis().SetTitleOffset( self.numerator.GetYaxis().GetTitleOffset()/padRatio )
+		if self.axisLabelSize==None:
+			self.axisLabelSize=self.ratioHistograms[-1].GetXaxis().GetLabelSize()
+		if self.axisTitleSize==None:
+			self.axisTitleSize=self.ratioHistograms[-1].GetXaxis().GetTitleSize();
+		if self.axisTitleOffset==None:
+			self.axisTitleOffset=self.ratioHistograms[-1].GetYaxis().GetTitleOffset()
+
+
+		for binNumber in range( 1, numberOfBins+1 ):
+			try : 
+				rateRatio=numeratorHistogram.GetBinContent(binNumber)/denominatorHistogram.GetBinContent(binNumber)
+				error=rateRatio*math.sqrt( math.pow(numeratorHistogram.GetBinError(binNumber)/numeratorHistogram.GetBinContent(binNumber),2) + math.pow(denominatorHistogram.GetBinError(binNumber)/denominatorHistogram.GetBinContent(binNumber),2) )
+			except ZeroDivisionError:
+				rateRatio = 0
+				error = 0
+			self.ratioHistograms[-1].SetBinContent( binNumber, rateRatio )
+			self.ratioHistograms[-1].SetBinError( binNumber, error )
+		# Work out the maximum and minimums
+		bin=self.ratioHistograms[-1].GetMaximumBin()
+		try: max=self.ratioHistograms[-1].GetBinContent(bin)+self.ratioHistograms[-1].GetBinErrorUp(bin)
+		except: max=self.ratioHistograms[-1].GetBinContent(bin)+self.ratioHistograms[-1].GetBinError(bin)
+		if max>self.maxiumumBinHeight: self.maxiumumBinHeight=max
+		bin=self.ratioHistograms[-1].GetMinimumBin()
+		try: min=self.ratioHistograms[-1].GetBinContent(bin)+self.ratioHistograms[-1].GetBinErrorDown(bin)
+		except: min=self.ratioHistograms[-1].GetBinContent(bin)+self.ratioHistograms[-1].GetBinError(bin)
+		if min<self.minimumBinHeight: self.minimumBinHeight=min
+
+
+	def draw(self):
+		self.pad.cd()
+		for index in range(0,len(self.ratioHistograms)):
+			self.ratioHistograms[index].SetMaximum( self.maxiumumBinHeight*1.2 )
+			self.ratioHistograms[index].SetMinimum( self.minimumBinHeight*0.8 )
+			self.ratioHistograms[index].GetYaxis().SetTitleSize( self.axisTitleSize );
+			self.ratioHistograms[index].GetXaxis().SetTitleSize( self.axisTitleSize );
+			self.ratioHistograms[index].GetYaxis().SetLabelSize( self.axisLabelSize );
+			self.ratioHistograms[index].GetXaxis().SetLabelSize( self.axisLabelSize );
+			self.ratioHistograms[index].GetYaxis().SetTitleOffset( self.axisTitleOffset );
+			if index==0: self.ratioHistograms[index].Draw()
+			else: self.ratioHistograms[index].Draw("same")
+		self.pad.Update()
+
+class TriggerRateComparisonPlot(object):
+	"""
+	Class that puts a TriggerRate object and TriggerRateRatio object on the same page.
+	"""
+	instantiationCount = 0 # Need this to get around root's stupid requirement for globally unique names
+
+	def __init__(self):
+		self.instantiationCount=TriggerRateComparisonPlot.instantiationCount
+		TriggerRateComparisonPlot.instantiationCount+=1
+
+		splitPoint=0.33
+		self.padRatio=(1-splitPoint)/splitPoint
+
+		self.canvas=TCanvas()
+		# Use the instantiation count to give every instance unique names for the pads
+		self.ratePad=TPad( "TriggerRateComparisonPlot("+str(self.instantiationCount)+").ratePad", "L1 rate", 0, splitPoint, 1, 1 )
+		self.ratioPad=TPad( "TriggerRateComparisonPlot("+str(self.instantiationCount)+").ratioPad", "L1 ratio", 0, 0, 1, splitPoint )
 		self.ratePad.Draw()
 		self.ratioPad.Draw()
-		self.ratePad.cd()
-		self.numerator.Draw()
-		self.denominator.Draw("same")
-		self.ratioPad.cd()
-		self.ratio.Draw()
+		self.ratioPad.SetTopMargin(0)
+		self.ratioPad.SetBottomMargin( self.ratePad.GetBottomMargin()*self.padRatio*1.1 ) # A little 10% extra because some is cut off
+		self.ratePad.SetBottomMargin(0.1) # was zero before I wanted to see the axis
+
+		self.ratePlot=TriggerRate(self.ratePad)
+		self.ratioPlot=TriggerRateRatio(self.ratioPad)
+		
 		
 
+	def add( self, histogram, legendName=None ):
+		self.ratePlot.add( histogram, legendName )
+		if len(self.ratePlot.histograms)>1:
+			self.ratioPlot.add( self.ratePlot.histograms[0], histogram )
+			
+	def addByTriggerName( self, file, triggerName, legendName=None ):
+		# Try the old format with the name used in L1Menu2015.C macro
+		histogram=file.Get( "h_"+triggerName+"_byThreshold" )
+		if histogram != None : return self.add( histogram, legendName )
+		
+		# Try the name given when there is more than one threshold and they're all scaled
+		histogram=file.Get( "L1_"+triggerName+"_v_allThresholdsScaled" )
+		if histogram != None : return self.add( histogram, legendName )
+		
+		# Try the name when there is only one threshold
+		histogram=file.Get( "L1_"+triggerName+"_v_threshold1" )
+		if histogram != None : return self.add( histogram, legendName )
+	
+	def draw(self):
+		self.ratePlot.axisTitleSize=0.053
+		self.ratioPlot.axisLabelSize=self.ratePlot.axisLabelSize*self.padRatio
+		self.ratioPlot.axisTitleSize=self.ratePlot.axisTitleSize*self.padRatio
+		self.ratioPlot.axisTitleOffset=self.ratePlot.axisTitleOffset/self.padRatio
+		self.ratePlot.draw()
+		self.ratioPlot.draw()
+		self.canvas.Update()
 
 
 
-newFile = ["New file",TFile.Open("/home/xtaldaq/CMSSWReleases/CMSSW_5_3_4/src/rateHistograms.root")]
-oldFile = ["Old file",TFile.Open("/home/xtaldaq/MenuGenerationFiles/oldResults/L1RateHist_14TeV100PU_25ns50bxMC_FallbackThr1_rates-modRates.root")]
-plots = []
 
 #triggers = ["SingleEG"]
 triggers = ["SingleEG","SingleIsoEG","SingleMu",
 "SingleIsoMu","SingleIsoTau","isoEG_EG","isoMu_Mu","isoTau_Tau",
 "isoEG_Mu","isoMu_EG","isoMu_Tau","SingleJetC","DoubleJet",
-#"SingleMu_CJet",
+"SingleMu_CJet",
 "SingleIsoEG_HTM","SingleMu_HTM","HTM","HTT",
-"QuadJetC","SixJet","SingleIsoEG_CJet","SingleTau","isoEG_Tau"
+"QuadJetC","SixJet","SingleIsoEG_CJet","SingleIsoEG_CJet2","SingleTau","isoEG_Tau"
 ]
 
-for trigger in triggers:
-	plots.append( TriggerRatePlot( newFile, oldFile, trigger ) )
-	#plots[-1].canvas.SaveAs( trigger+"_rateVsThreshold.pdf" )
+plots = []
 
+#newFile=TFile.Open("/home/xtaldaq/CMSSWReleases/CMSSW_5_3_4/src/rateHistograms-logicTest.root")
+#oldFile=TFile.Open("/home/xtaldaq/MenuGenerationFiles/oldResults/L1RateHist_14TeV100PU_25ns50bxMC_FallbackThr1_rates-newBinning.root")
+newFile=TFile.Open("/home/xtaldaq/MenuGenerationFiles/oldResults/rateHistograms-fullSample.root")
+oldFile=TFile.Open("/home/xtaldaq/CMSSWReleases/CMSSW_5_3_4/src/rateHistograms.root")
+newFileTitle="FullSample file"
+oldFileTitle="ReducedSample file"
+
+for trigger in triggers:
+	plots.append( TriggerRateComparisonPlot() )
+#	if trigger!="SingleIsoEG_CJet":
+#		plots[-1].addByTriggerName( newFile, trigger, newFileTitle )
+#	else:
+#		plots[-1].add( newFile.Get("L1_SingleIsoEG_CJet2_v_allThresholdsScaled"), newFileTitle )
+	plots[-1].addByTriggerName( newFile, trigger, newFileTitle )
+	plots[-1].addByTriggerName( oldFile, trigger, oldFileTitle )
+	plots[-1].draw()
+	# Add an extra member to say where to save, in case I choose to do so later
+	plots[-1].saveFilename=trigger+"_rateVsThreshold.pdf"
+	#plots[-1].canvas.SaveAs( trigger+"_rateVsThreshold.pdf" )
 
